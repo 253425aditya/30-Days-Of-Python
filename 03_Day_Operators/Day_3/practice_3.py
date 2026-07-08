354f397e9fd636171ef740a9ea778f3804605758
253425aditya <vermaaditya8624@gmail.com>
2026-07-08 15:35:55 +0530

    Added perameter of triangle check

--- 03_Day_Operators/Day_3/practice_3.py
+++ 03_Day_Operators/Day_3/practice_3.py
@@ -6,5 +6,9 @@ base = int(input("Enter Base of Triangle: "))
 heightt = int(input("Enter height of Triangle "))
 print("The area of Triangle is : ",(0.5)*base*heightt)
 
+a = int(input("Enter a number : "))
+b = int(input("Enter a number : "))
+c = int(input("Enter a number : "))
+print("The perimeter of the triangel is ", a+b+c)
 
 
