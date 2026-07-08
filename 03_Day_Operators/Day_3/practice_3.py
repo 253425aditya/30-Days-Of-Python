3095f0acf9ac3ebb2843d46baa8c925942e21fd2
253425aditya <vermaaditya8624@gmail.com>
2026-07-08 15:34:54 +0530

    indentation fix

--- 03_Day_Operators/Day_3/practice_3.py
+++ 03_Day_Operators/Day_3/practice_3.py
@@ -3,7 +3,8 @@ height = 175
 comp = 1 + 2j
 
 base = int(input("Enter Base of Triangle: "))
-heightt = int(input("Enter height of Triangle"))
+heightt = int(input("Enter height of Triangle "))
 print("The area of Triangle is : ",(0.5)*base*heightt)
+
 
 
