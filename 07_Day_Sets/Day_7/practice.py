9d357858256b650d47a88d7616ee210c1b6fd0cc
253425aditya <vermaaditya8624@gmail.com>
2026-07-13 14:34:53 +0530

    Exercises: Level 2 completed

--- 07_Day_Sets/Day_7/practice.py
+++ 07_Day_Sets/Day_7/practice.py
@@ -9,4 +9,15 @@ it_companies.add('Twitter')
 it_companies.update(['X','PTI','CFT'])
 it_companies.remove('Apple')
 it_companies.discard('Apple')
+#when you use discard it will not give an error if the item is not present in the set but remove will give an error if the item is not present in the set
 print(it_companies)
+
+joined = A | B
+inter = A & B
+print(A.issubset(B))
+print(A.isdisjoint(B))
+diff = B - A
+print(diff)
+
+del A
+del B
