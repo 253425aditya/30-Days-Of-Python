15c9b89418c3d2a6561b77aa83209ef207ae2059
253425aditya <vermaaditya8624@gmail.com>
2026-07-13 14:45:56 +0530

    Exercises: Level completed

--- 07_Day_Sets/Day_7/practice.py
+++ 07_Day_Sets/Day_7/practice.py
@@ -21,3 +21,14 @@ print(diff)
 
 del A
 del B
+
+age_set = set(age)
+print(f"List length: {len(age)}\nSet lenght: {len(age_set)}")
+
+s1 = 'String is sequance of character stored at contigeous memory location and they are immutable.'
+l1 = 'List is an use to store multiple data under single name and they can contain duplicate value and they are mutable.'
+t1 = 'Tuple is same as list but just they are immutable.'
+se1 = 'Set is unordered collection of elements and they are used to store unique elements.'
+
+setWords = set([s1,l1,t1,se1])
+print(setWords)
