1aea2c78c6c515e880b64682811e515ca5c50a75
253425aditya <vermaaditya8624@gmail.com>
2026-07-13 14:00:09 +0530

    Exercises: Level 2 Copleted

--- 06_Day_Tuples/Day_6/practice.py
+++ 06_Day_Tuples/Day_6/practice.py
@@ -1,3 +1,5 @@
+#Exercises: Level 1
+
 tp1 = ()
 t1 = ('Giang','Sol')
 t2 = ('Helios','Sun')
@@ -6,3 +8,23 @@ l1 = list(t1)
 l1.append('Hydra')
 t1 = tuple(l1)
 print(t3)
+
+#Exercises: Level 2
+
+a,b,c = t1
+print(a,b,c)
+fruits = ('banana','orange','mango','lemon')
+vegetables = ('Tomato','Potato','Cabbage','Onion','Carrot')
+animals = ('Tiger','Lion','Puma','Leopard')
+
+food_studd_it = fruits + vegetables + animals
+print(food_studd_it)
+print(food_studd_it[len(food_studd_it)//2])
+print(food_studd_it[:3])
+print(food_studd_it[-3:])
+
+del food_studd_it
+
+nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
+print('Estonia' in nordic_countries)
+print('Iceland' in nordic_countries)
