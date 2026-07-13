8f10a6a2c8ee015a666446485d7b1e6e1a396aac
253425aditya <vermaaditya8624@gmail.com>
2026-07-13 13:24:24 +0530

    final commit of day_5

--- 05_Day_Lists/Day_5/practice.py
+++ 05_Day_Lists/Day_5/practice.py
@@ -1,22 +1,264 @@
-lst = []
-fruits = ['banana', 'orange', 'mango', 'lemon','apple']
-leng = len(fruits)
+# lst = []
+# fruits = ['banana', 'orange', 'mango', 'lemon','apple']
+# leng = len(fruits)
 
-print(fruits[0],fruits[leng//2],fruits[leng-1])
+# print(fruits[0],fruits[leng//2],fruits[leng-1])
 
-mixed_data_types = ['Aditya',100,176,'Single']
+# mixed_data_types = ['Aditya',100,176,'Single']
 
-it_companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+# it_companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
 
-print(it_companies)
-print(len(it_companies))
-print(it_companies[0],it_companies[leng//2],it_companies[leng-1])
-it_companies[0] = 'Baka pvt ltd.'
-print(it_companies)
-it_companies.append('Facebook')
-print(it_companies)
-it_companies[0] = it_companies[0].upper()
-print(it_companies)
-join_it = '#'.join(it_companies)
-print(join_it)
-print('facebook' in it_companies)
+# print(it_companies)
+# print(len(it_companies))
+# print(it_companies[0],it_companies[leng//2],it_companies[leng-1])
+# it_companies[0] = 'Baka pvt ltd.'
+# print(it_companies)
+# it_companies.append('Facebook')
+# print(it_companies)
+# it_companies[0] = it_companies[0].upper()
+# print(it_companies)
+# join_it = '#'.join(it_companies)
+# print(join_it)
+# print('facebook' in it_companies)
+
+# it_companies.sort(reverse=True)
+# print(it_companies)
+
+# it_companies.pop(len(it_companies)-1)
+# print(it_companies)
+# it_companies.clear()
+
+# print(it_companies)
+
+# del it_companies
+# #print(it_companies)
+
+# front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+# back_end = ['Node','Express', 'MongoDB']
+
+# full = front_end+back_end
+# full_stack = full.copy()
+# print(full)
+
+# ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+# ages.sort()
+# print(f"Min age: {ages[0]} \nMax age: {ages[len(ages)-1]} \nmedian age: {ages[len(ages)//2]}")
+# sum = 0
+# for i in ages :
+#     sum = sum+i
+# print(sum/len(ages))
+# avg = sum//len(ages)
+
+# min_avg = abs(ages[0]-avg)
+
+# max_avg = abs(ages[len(ages)-1]-avg)
+# print(f"{min_avg} \n {max_avg}")
+
+countries = [
+  'Afghanistan',
+  'Albania',
+  'Algeria',
+  'Andorra',
+  'Angola',
+  'Antigua and Barbuda',
+  'Argentina',
+  'Armenia',
+  'Australia',
+  'Austria',
+  'Azerbaijan',
+  'Bahamas',
+  'Bahrain',
+  'Bangladesh',
+  'Barbados',
+  'Belarus',
+  'Belgium',
+  'Belize',
+  'Benin',
+  'Bhutan',
+  'Bolivia',
+  'Bosnia and Herzegovina',
+  'Botswana',
+  'Brazil',
+  'Brunei',
+  'Bulgaria',
+  'Burkina Faso',
+  'Burundi',
+  'Cabo Verde',
+  'Cambodia',
+  'Cameroon',
+  'Canada',
+  'Central African Republic',
+  'Chad',
+  'Chile',
+  'China',
+  'Colombia',
+  'Comoros',
+  'Congo, Democratic Republic of the',
+  'Congo, Republic of the',
+  'Costa Rica',
+  "Côte d'Ivoire",
+  'Croatia',
+  'Cuba',
+  'Cyprus',
+  'Czech Republic',
+  'Denmark',
+  'Djibouti',
+  'Dominica',
+  'Dominican Republic',
+  'East Timor (Timor-Leste)',
+  'Ecuador',
+  'Egypt',
+  'El Salvador',
+  'Equatorial Guinea',
+  'Eritrea',
+  'Estonia',
+  'Eswatini',
+  'Ethiopia',
+  'Fiji',
+  'Finland',
+  'France',
+  'Gabon',
+  'Gambia',
+  'Georgia',
+  'Germany',
+  'Ghana',
+  'Greece',
+  'Grenada',
+  'Guatemala',
+  'Guinea',
+  'Guinea-Bissau',
+  'Guyana',
+  'Haiti',
+  'Honduras',
+  'Hungary',
+  'Iceland',
+  'India',
+  'Indonesia',
+  'Iran',
+  'Iraq',
+  'Ireland',
+  'Israel',
+  'Italy',
+  'Jamaica',
+  'Japan',
+  'Jordan',
+  'Kazakhstan',
+  'Kenya',
+  'Kiribati',
+  'Korea, North',
+  'Korea, South',
+  'Kuwait',
+  'Kyrgyzstan',
+  'Laos',
+  'Latvia',
+  'Lebanon',
+  'Lesotho',
+  'Liberia',
+  'Libya',
+  'Liechtenstein',
+  'Lithuania',
+  'Luxembourg',
+  'Madagascar',
+  'Malawi',
+  'Malaysia',
+  'Maldives',
+  'Mali',
+  'Malta',
+  'Marshall Islands',
+  'Mauritania',
+  'Mauritius',
+  'Mexico',
+  'Micronesia',
+  'Moldova',
+  'Monaco',
+  'Mongolia',
+  'Montenegro',
+  'Morocco',
+  'Mozambique',
+  'Myanmar',
+  'Namibia',
+  'Nauru',
+  'Nepal',
+  'Netherlands',
+  'New Zealand',
+  'Nicaragua',
+  'Niger',
+  'Nigeria',
+  'North Macedonia',
+  'Norway',
+  'Oman',
+  'Pakistan',
+  'Palau',
+  'Palestine',
+  'Panama',
+  'Papua New Guinea',
+  'Paraguay',
+  'Peru',
+  'Philippines',
+  'Poland',
+  'Portugal',
+  'Qatar',
+  'Romania',
+  'Russia',
+  'Rwanda',
+  'Saint Kitts and Nevis',
+  'Saint Lucia',
+  'Saint Vincent and the Grenadines',
+  'Samoa',
+  'San Marino',
+  'Sao Tome and Principe',
+  'Saudi Arabia',
+  'Senegal',
+  'Serbia',
+  'Seychelles',
+  'Sierra Leone',
+  'Singapore',
+  'Slovakia',
+  'Slovenia',
+  'Solomon Islands',
+  'Somalia',
+  'South Africa',
+  'South Sudan',
+  'Spain',
+  'Sri Lanka',
+  'Sudan',
+  'Suriname',
+  'Sweden',
+  'Switzerland',
+  'Syria',
+  'Tajikistan',
+  'Tanzania',
+  'Thailand',
+  'Togo',
+  'Tonga',
+  'Trinidad and Tobago',
+  'Tunisia',
+  'Turkey',
+  'Turkmenistan',
+  'Tuvalu',
+  'Uganda',
+  'Ukraine',
+  'United Arab Emirates',
+  'United Kingdom',
+  'United States',
+  'Uruguay',
+  'Uzbekistan',
+  'Vanuatu',
+  'Vatican City',
+  'Venezuela',
+  'Vietnam',
+  'Yemen',
+  'Zambia',
+  'Zimbabwe'
+];
+
+length = len(countries)
+print(length)
+mid = length//2
+if(length%2!=0):
+    mid = mid+1
+first_half = countries[:mid]
+second_half = countries[mid:]
+print(len(first_half+second_half))
+print(len(first_half))
+print(len(second_half))
