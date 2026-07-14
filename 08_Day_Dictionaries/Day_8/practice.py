a0f287d58e7ec94c03503686d28a4143fe79bd4c
253425aditya <vermaaditya8624@gmail.com>
2026-07-14 19:02:45 +0530

    Solved 5 problems from exercise'

---
+++ 08_Day_Dictionaries/Day_8/practice.py
@@ -0,0 +1,21 @@
+dog = {}
+dog['name'] = 'G'
+dog['color'] = 'White'
+dog['legs'] = 2
+dog['age'] = 20
+
+student = {
+    'first_name': 'John',
+    'last_name': 'Doe',
+    'gender': 'Male',
+    'age': 25,
+    'marital_status': 'Single',
+    'skills': ['Python', 'JavaScript', 'SQL'],
+    'country': 'USA',
+    'city': 'New York'
+}
+
+print(len(dog))
+
+print(student['skills'])
+print(type(student['skills']))
