426c78b0c196ce71a8e7e2dce038bc12d1f91d5d
253425aditya <vermaaditya8624@gmail.com>
2026-07-15 18:33:19 +0530

    Done with Status checker

--- 09_Day_Conditionals/Dat_9/practice.py
+++ 09_Day_Conditionals/Dat_9/practice.py
@@ -23,20 +23,62 @@
 # else :
 #     print('Fail')
 
-Autumn = ['September', 'October', 'November']
-Winter = ['December', 'January', 'February']
-Spring  = ['March', 'April', 'May']
-Summer = ['June', 'July', 'August']
+# Autumn = ['September', 'October', 'November']
+# Winter = ['December', 'January', 'February']
+# Spring  = ['March', 'April', 'May']
+# Summer = ['June', 'July', 'August']
 
-season = input("Enter a Season : ")
-season = season.capitalize()
-if season in Autumn:
-    print("Season is Autumn")
-elif season in Winter:
-    print("Season is Winter")
-elif season in Spring:
-    print("Season is Spring")
-elif season in Summer:
-    print("Season is Summer")
-else:
-    print("Enter Valid Month name")
+# season = input("Enter a Season : ")
+# season = season.capitalize()
+# if season in Autumn:
+#     print("Season is Autumn")
+# elif season in Winter:
+#     print("Season is Winter")
+# elif season in Spring:
+#     print("Season is Spring")
+# elif season in Summer:
+#     print("Season is Summer")
+# else:
+#     print("Enter Valid Month name")
+
+person = {
+    'first_name': 'Asabeneh',
+    'last_name': 'Yetayeh',
+    'age': 250,
+    'country': 'Finland',
+    'is_married': True,
+    'skills': ['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address': {
+        'street': 'Space street',
+        'zipcode': '02210'
+    }
+}
+
+if 'skills' in person:
+    skills = person['skills']
+    middle_skill = skills[len(skills) // 2]
+    print("Middle skill:", middle_skill)
+
+if 'skills' in person:
+    print("Has Python skill:", 'Python' in person['skills'])
+
+if 'skills' in person:
+    skills = person['skills']
+
+    if all(skill in skills for skill in ['JavaScript', 'React']) and len(skills) == 2:
+        print("He is a front end developer")
+
+    elif all(skill in skills for skill in ['Node', 'Python', 'MongoDB']):
+        print("He is a backend developer")
+
+    elif all(skill in skills for skill in ['React', 'Node', 'MongoDB']):
+        print("He is a fullstack developer")
+
+    else:
+        print("Unknown title")
+
+if person['is_married'] and person['country'] == 'Finland':
+    print(
+        f"{person['first_name']} {person['last_name']} lives in "
+        f"{person['country']}. He is married."
+    )
