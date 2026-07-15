9f5eeb5eabd74ea0a4cc6f563693e6bfa58b14a7
253425aditya <vermaaditya8624@gmail.com>
2026-07-15 18:15:12 +0530

    Grade checker DONE

--- 09_Day_Conditionals/Dat_9/practice.py
+++ 09_Day_Conditionals/Dat_9/practice.py
@@ -4,10 +4,21 @@
 # else :
 #     print(f"You need {18-age} more years to learn to drive.")
 
-num1 = int(input("Enter your num1: "))
-num2 = int(input("Enter your num2: "))
-if num1 > num2 :
-    print("Number 1 is greater Than number 2")
-else :
-    print("Number 2 is greater Than number 1")
+# num1 = int(input("Enter your num1: "))
+# num2 = int(input("Enter your num2: "))
+# if num1 > num2 :
+#     print("Number 1 is greater Than number 2")
+# else :
+#     print("Number 2 is greater Than number 1")
 
+score = int(input("Enter you score : "))
+if score >= 90 :
+    print('A')
+elif score >= 80 :
+    print('B')
+elif score >= 70 :
+    print('C')
+elif score >= 60 :
+    print('D')
+else :
+    print('Fail')
