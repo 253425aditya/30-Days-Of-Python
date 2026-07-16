379c4868b4c623c73c8d8a9e047b0c24daa595e3
253425aditya <vermaaditya8624@gmail.com>
2026-07-16 15:24:55 +0530

    Done wit Excercise Level 1

---
+++ 10_Day_Loops/Day_10/practice.py
@@ -0,0 +1,40 @@
+for i in range(10,0,-1):
+    print(i)
+
+for i in range(1,8):
+    for i in range(i):
+        print('#',end="")
+    print()
+
+
+print("\n\n\n\n")
+
+for i in range(10):
+    for j in range(10):
+        print('# ',end="")
+    print()
+
+print("\n")
+
+for i in range(11):
+    print(f"{i} x {i} = {i*i}")
+
+print("\n")
+
+lst = ['Python', 'Numpy','Pandas','Django', 'Flask']
+for i in lst:
+    print(i)
+
+print("\n")
+
+for i in range(101):
+    if i%2==0:
+        print(i)
+
+print("\n")
+
+for i in range(101):
+    if i%2!=0:
+        print(i)
+
+      
