0a83483c498dbde979833392937a42301176a89a
253425aditya <vermaaditya8624@gmail.com>
2026-07-17 15:25:58 +0530

    Practiced level 1

---
+++ 12_Day_Modules/Day_12/practice.py
@@ -0,0 +1,27 @@
+# main.py file
+# import mymodule
+# print(mymodule.generate_full_name('Asabeneh', 'Yetayeh')) # Asabeneh Yetayeh
+
+import random
+import string
+
+#print(random.randint(100000,999999))
+
+
+
+def user_id_gen_by_user(num1,len_id):
+    for i in range(num1):
+        username = ''.join(random.choices(string.ascii_lowercase + string.digits, k=len_id))
+        print(username)
+
+def rgb_color_gen():
+    parts = []
+    for _ in range(3):
+        value = random.randint(0, 255)  
+        parts.append(str(value))        
+    rgb = ','.join(parts)               
+    print(rgb)
+
+
+rgb_color_gen()
+
