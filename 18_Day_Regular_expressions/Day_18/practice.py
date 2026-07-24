e9783448bb00c06587a49b76f7a9802bd81d5f5a
253425aditya <vermaaditya8624@gmail.com>
2026-07-24 17:16:22 +0530

    Done with level 3

--- 18_Day_Regular_expressions/Day_18/practice.py
+++ 18_Day_Regular_expressions/Day_18/practice.py
@@ -14,9 +14,42 @@ import re
 # max_word = max(split_para, key=split_para.get)
 # print(f"The most frequent word is '{max_word}' with {split_para[max_word]} occurrences.")
 
-para = 'The position of some particles on the horizontal x-axis are -12, -4, -3 and -1 in the negative direction, 0 at origin, 4 and 8 in the positive direction. Extract these numbers from this whole text and find the distance between the two furthest particles.'
+# para = 'The position of some particles on the horizontal x-axis are -12, -4, -3 and -1 in the negative direction, 0 at origin, 4 and 8 in the positive direction. Extract these numbers from this whole text and find the distance between the two furthest particles.'
 
-matches = re.findall('-?\d+',para)
-new = [int(i) for i in matches]
-matches.sort()
-print(new)
+# matches = re.findall('-?\d+',para)
+# new = [int(i) for i in matches]
+# matches.sort()
+# print(new)
+
+# LEVEL-2
+
+# def is_valid_variable(name):
+#     pattern = r'^[a-zA-Z][a-zA-Z0-9_]*$'
+#     if re.match(pattern,name):
+#         return True
+#     else:
+#         return False
+
+# print(is_valid_variable('first_nam9e'))
+
+
+
+# level - 3
+sentence = '''%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?'''
+
+
+cleaned_text = re.sub(r'[%@&$#;]','',sentence)
+print(cleaned_text)
+
+new_para = re.findall('\w+',cleaned_text)
+
+split_para = {}
+
+for word in new_para:
+    if word in split_para:
+        split_para[word] = split_para[word] + 1
+    else:
+        split_para[word] = 1 
+
+max_word = max(split_para, key=split_para.get)
+print(f"The most frequent word is '{max_word}' with {split_para[max_word]} occurrences.")
--- 18_Day_Regular_expressions/Day_18/tempCodeRunnerFile.py
+++ 18_Day_Regular_expressions/Day_18/tempCodeRunnerFile.py
@@ -1,7 +1,1 @@
-span = match.span()
-# print(span)     # (0, 15)
-# # Lets find the start and stop position from the span
-# start, end = span
-# print(start, end)  # 0 15
-# substring = txt[start:end]
-# print(substring)       # I love to teach
+first_name
