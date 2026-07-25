6767f03c791d6dcd92fd4f750f73cf8802f3f9b2
253425aditya <vermaaditya8624@gmail.com>
2026-07-25 15:11:39 +0530

    Extracted E-mails from data using re

--- 19_Day_File_handling/Day_19/practice.py
+++ 19_Day_File_handling/Day_19/practice.py
@@ -1,5 +1,6 @@
 import os
 import json
+import re
 
 # with open(r"C:\Users\Hp\Downloads\michelle_obama_speech.txt") as f:
 #     text = f.read()
@@ -24,22 +25,37 @@ import json
 #     )
 #     return sorted_words
 
-with open(r"C:\Users\Hp\Downloads\countries_data.json", encoding="utf-8") as f:
-    text = f.read()
+# with open(r"C:\Users\Hp\Downloads\countries_data.json", encoding="utf-8") as f:
+#     text = f.read()
 
-data = json.loads(text)
+# data = json.loads(text)
 
 # print(most_spoken_lang_list(data,10))
 
-def most_populated_countries(data,num):
-    pop = {}
-    for i in data:
-        pop[i['name']] = i['population']
+# def most_populated_countries(data,num):
+#     pop = {}
+#     for i in data:
+#         pop[i['name']] = i['population']
 
-    sorted_pop = dict(
-    sorted(pop.items(), key=lambda item: item[1], reverse=True)[:num]
-    )
-    return sorted_pop
+#     sorted_pop = dict(
+#     sorted(pop.items(), key=lambda item: item[1], reverse=True)[:num]
+#     )
+#     return sorted_pop
 
 
-print(most_populated_countries(data,10))
+# print(most_populated_countries(data,10))
+
+'''Level 2'''
+
+with open(r"C:\Users\Hp\Downloads\email_exchanges_big.txt", encoding='utf-8') as f:
+    text = f.read()
+
+emails = re.findall(r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}", text)
+
+# print(emails)
+
+print("Total emails:", len(emails))
+unique_emails = list(set(emails))
+
+# print(unique_emails)
+print("Unique emails:", len(unique_emails))
