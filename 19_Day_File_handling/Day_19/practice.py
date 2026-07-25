160bace7adae36ee2e4161532cdceb11e5c1f982
253425aditya <vermaaditya8624@gmail.com>
2026-07-25 14:48:42 +0530

    Created Function to get top 10 most populated countries

--- 19_Day_File_handling/Day_19/practice.py
+++ 19_Day_File_handling/Day_19/practice.py
@@ -10,23 +10,36 @@ import json
 # print("Lines:", num_lines)
 # print("Words:", num_words)
 
-def most_spoken_lang_list(data,num):
-    lang = {}
-    for i in data:
-        for l in i['languages']:
-            if l in lang:
-                lang[l] += 1
-            else:
-                lang[l] = 1
+# def most_spoken_lang_list(data,num):
+#     lang = {}
+#     for i in data:
+#         for l in i['languages']:
+#             if l in lang:
+#                 lang[l] += 1
+#             else:
+#                 lang[l] = 1
 
-    sorted_words = dict(
-    sorted(lang.items(), key=lambda item: item[1], reverse=True)[:num]
-    )
-    return sorted_words
+#     sorted_words = dict(
+#     sorted(lang.items(), key=lambda item: item[1], reverse=True)[:num]
+#     )
+#     return sorted_words
 
 with open(r"C:\Users\Hp\Downloads\countries_data.json", encoding="utf-8") as f:
     text = f.read()
 
 data = json.loads(text)
 
-print(most_spoken_lang_list(data,10))
+# print(most_spoken_lang_list(data,10))
+
+def most_populated_countries(data,num):
+    pop = {}
+    for i in data:
+        pop[i['name']] = i['population']
+
+    sorted_pop = dict(
+    sorted(pop.items(), key=lambda item: item[1], reverse=True)[:num]
+    )
+    return sorted_pop
+
+
+print(most_populated_countries(data,10))
--- 19_Day_File_handling/Day_19/tempCodeRunnerFile.py
+++ 19_Day_File_handling/Day_19/tempCodeRunnerFile.py
@@ -1,6 +1,1 @@
-import os
-
-with open(r"C:\Users\Hp\Downloads\michelle_obama_speech.txt") as f:
-    lines = f.readlines()
-    text = f.read()
-print(text)
+data = json.loads(text)
