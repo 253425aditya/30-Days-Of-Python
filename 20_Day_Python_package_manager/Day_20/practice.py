11ec9c7cab5d67e020b6977a8afbcee75cd99ddc
253425aditya <vermaaditya8624@gmail.com>
2026-07-26 15:53:04 +0530

    Fetching data from website using requests module

--- 20_Day_Python_package_manager/Day_20/practice.py
+++ 20_Day_Python_package_manager/Day_20/practice.py
@@ -1,13 +1,9 @@
-import webbrowser # web browser module to open websites
+import requests # importing the request module
 
-# list of urls: python
-url_lists = [
-    'http://www.python.org',
-    'https://www.linkedin.com/in/asabeneh/',
-    'https://github.com/Asabeneh',
-    'https://twitter.com/Asabeneh',
-]
+url = 'https://takeuforward.org/dsa/strivers-a2z-sheet-learn-dsa-a-to-z' # text from a website
 
-# opens the above list of websites in a different tab
-for url in url_lists:
-    webbrowser.open_new_tab(url)
+response = requests.get(url) # opening a network and fetching a data
+print(response)
+print(response.status_code) # status code, success:200
+print(response.headers)     # headers information
+print(response.text) # gives all the text from the page
